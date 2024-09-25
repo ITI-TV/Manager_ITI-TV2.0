841a75a3bfc4d6313ef70f12983f2565fb78f535
ZELA2000 <79649375+ZELA2000@users.noreply.github.com>
2024-09-25 18:20:38 +0200

    Risoluzione issue 11

--- js/Inserisci Componente Aggiuntivo/main.js
+++ js/Inserisci Componente Aggiuntivo/main.js
@@ -4,63 +4,63 @@ function startInserimentoComponenteAggiuntivo() {
     document.getElementById('formTestoComponentiAggiuntivi').addEventListener('input', checkLunghezzaCorpo);
     document.getElementById('formDataInizioComponentiAggiuntivi').addEventListener('change', checkDateInizio);
     document.getElementById('formDataFineComponentiAggiuntivi').addEventListener('change', checkDateFine);
-    document.getElementById('formImmagineComponentiAggiuntivi').addEventListener('change', function(){
+    document.getElementById('formImmagineComponentiAggiuntivi').addEventListener('change', function () {
         document.getElementById('ImmagineComponentiAggiuntivi').src = URL.createObjectURL(this.files[0]);
     });
     document.getElementById('submitComponenteAggiuntivo').addEventListener('click', invioComponenteAggiuntivo);
 }
 
-function checkLunghezzaTitolo(){
+function checkLunghezzaTitolo() {
     document.getElementById('formTitoloComponentiAggiuntivi').classList.remove('error');
-    if(document.getElementById('formTitoloComponentiAggiuntivi').value.length > 100){
+    if (document.getElementById('formTitoloComponentiAggiuntivi').value.length > 100) {
         document.getElementById('formTitoloComponentiAggiuntivi').value = document.getElementById('formTitoloComponentiAggiuntivi').value.substring(0, 100);
     }
-    if(document.getElementById('formTitoloComponentiAggiuntivi').value.length < 1) {
+    if (document.getElementById('formTitoloComponentiAggiuntivi').value.length < 1) {
         document.getElementById('formTitoloComponentiAggiuntivi').classList.add('error');
     }
     document.getElementById('textFormTitoloComponentiAggiuntivi').innerHTML = document.getElementById('formTitoloComponentiAggiuntivi').value.length + '/100';
     document.getElementById('TitoloComponentiAggiuntivi').innerHTML = document.getElementById('formTitoloComponentiAggiuntivi').value;
 }
 
-function checkLunghezzaCorpo(){
+function checkLunghezzaCorpo() {
     document.getElementById('formTestoComponentiAggiuntivi').classList.remove('error');
-    if(document.getElementById('formTestoComponentiAggiuntivi').value.length > 625){
+    if (document.getElementById('formTestoComponentiAggiuntivi').value.length > 625) {
         document.getElementById('formTestoComponentiAggiuntivi').value = document.getElementById('formTestoComponentiAggiuntivi').value.substring(0, 625);
     }
-    if(document.getElementById('formTestoComponentiAggiuntivi').value.length < 1) {
+    if (document.getElementById('formTestoComponentiAggiuntivi').value.length < 1) {
         document.getElementById('formTestoComponentiAggiuntivi').classList.add('error');
     }
     document.getElementById('textFormTestoComponentiAggiuntivi').innerHTML = document.getElementById('formTestoComponentiAggiuntivi').value.length + '/625';
     document.getElementById('TestoComponentiAggiuntivi').innerHTML = document.getElementById('formTestoComponentiAggiuntivi').value;
 }
 
-function checkDateFine(){
-    if(document.getElementById('formDataFineComponentiAggiuntivi').value < document.getElementById('formDataInizioComponentiAggiuntivi').value){
+function checkDateFine() {
+    if (document.getElementById('formDataFineComponentiAggiuntivi').value < document.getElementById('formDataInizioComponentiAggiuntivi').value) {
         document.getElementById('formDataInizioComponentiAggiuntivi').value = document.getElementById('formDataFineComponentiAggiuntivi').value;
     }
 }
 
-function checkDateInizio(){
-    if(document.getElementById('formDataInizioComponentiAggiuntivi').value > document.getElementById('formDataFineComponentiAggiuntivi').value){
+function checkDateInizio() {
+    if (document.getElementById('formDataInizioComponentiAggiuntivi').value > document.getElementById('formDataFineComponentiAggiuntivi').value) {
         document.getElementById('formDataFineComponentiAggiuntivi').value = document.getElementById('formDataInizioComponentiAggiuntivi').value;
     }
 }
 
-function invioComponenteAggiuntivo(){
+function invioComponenteAggiuntivo() {
     checkSession(function (isLoggedIn) {
-        if (isLoggedIn){
-            if(document.getElementById('formTitoloComponentiAggiuntivi').value.length < 1 ||
+        if (isLoggedIn) {
+            if (document.getElementById('formTitoloComponentiAggiuntivi').value.length < 1 ||
                 document.getElementById('formTestoComponentiAggiuntivi').value.length < 1) {
                 alert('Compilare tutti i campi');
                 return;
             }
 
-            if(document.getElementById('formImmagineComponentiAggiuntivi').files.length < 1){
+            if (document.getElementById('formImmagineComponentiAggiuntivi').files.length < 1) {
                 alert('Caricare un\'immagine');
                 return;
             }
 
-            if(document.getElementById('formDataInizioComponentiAggiuntivi').value.length < 1 || document.getElementById('formDataFineComponentiAggiuntivi').value.length < 1){
+            if (document.getElementById('formDataInizioComponentiAggiuntivi').value.length < 1 || document.getElementById('formDataFineComponentiAggiuntivi').value.length < 1) {
                 alert('Inserire una data di inizio e di fine');
                 return;
             }
@@ -72,9 +72,10 @@ function invioComponenteAggiuntivo(){
             let xhr = new XMLHttpRequest();
             xhr.open('POST', 'php/uploadImage.php', true);
 
-            xhr.onload = function() {
+            xhr.onload = function () {
                 if (xhr.status === 200) {
                     let response = xhr.responseText;
+                    console.log(response)
 
                     $.post('php/setters.php', {
                         Titolo: document.getElementById('formTitoloComponentiAggiuntivi').value,
@@ -85,7 +86,7 @@ function invioComponenteAggiuntivo(){
                         Firma: firmaComponenteAggiuntivo,
                         action: 'setComponenteAggiuntivo'
                     })
-                        .done(function(response) {
+                        .done(function (response) {
                             alert('Componente aggiuntivo inserito correttamente');
                             document.getElementById('formTitoloComponentiAggiuntivi').value = '';
                             document.getElementById('formTestoComponentiAggiuntivi').value = '';
@@ -97,7 +98,7 @@ function invioComponenteAggiuntivo(){
                             document.getElementById('TitoloComponentiAggiuntivi').innerHTML = '';
                             document.getElementById('TestoComponentiAggiuntivi').innerHTML = '';
                         })
-                        .fail(function(error) {
+                        .fail(function (error) {
                             console.error('Errore nella query:', error);
                         });
                 } else {
@@ -105,7 +106,7 @@ function invioComponenteAggiuntivo(){
                 }
             };
 
-            xhr.onerror = function() {
+            xhr.onerror = function () {
                 console.error('Errore nel caricamento dell\'immagine:', xhr.statusText);
             };
 
