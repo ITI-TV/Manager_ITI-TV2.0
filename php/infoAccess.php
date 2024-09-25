76dd742140c0ad6e7e97a353252b3780ef17f1fc
ZELA2000 <79649375+ZELA2000@users.noreply.github.com>
2024-09-25 18:33:46 +0200

    Update infoAccess.php

--- php/infoAccess.php
+++ php/infoAccess.php
@@ -1,6 +1,6 @@
 <?php
-$serverConn="192.168.1.147";
-$usernameConn="ititv";
-$passwordConn="ititv";
-$dbnameConn="ititv";
+$serverConn="DBMS IP";
+$usernameConn="DBMS USERNAME";
+$passwordConn="DBMS PASSWORD";
+$dbnameConn="DBMS DB NAME";
 
