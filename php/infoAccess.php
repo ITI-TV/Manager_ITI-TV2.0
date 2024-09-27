b9bb2031c9c32c0f2001a8b977dbe463485df8eb
ZELA2000 <79649375+ZELA2000@users.noreply.github.com>
2024-09-27 18:05:37 +0200

    Merge branch 'master' into 7-grafica-responsive-manager
    # Conflicts:
    #	.gitignore
    #	php/infoAccess.php

--- php/infoAccess.php
+++ php/infoAccess.php
@@ -1,13 +1,6 @@
 <?php
-<<<<<<< HEAD
 $serverConn="localhost";
 $usernameConn="ititv";
 $passwordConn="ititv";
 $dbnameConn="ititv";
-=======
-$serverConn="DBMS IP";
-$usernameConn="DBMS USERNAME";
-$passwordConn="DBMS PASSWORD";
-$dbnameConn="DBMS DB NAME";
->>>>>>> master
 
